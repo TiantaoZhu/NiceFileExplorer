a2f4a7645549b5a4ec98a606121a433256a24c7f
DrSlark <zttbill@163.com>
2015-10-23 17:41:49 +0800

    11
    11

---
+++ app/src/main/java/com/drslark/nicefileexplore/delegate/OnBackPressed.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (C) 2015 Baidu, Inc. All Rights Reserved.
+ */
+package com.drslark.nicefileexplore.delegate;
+
+/**
+ * Created by zhutiantao on 2015/10/22.
+ */
+public interface OnBackPressed {
+    void onBackPressed();
+}
